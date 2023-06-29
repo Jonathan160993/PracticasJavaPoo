cfa415b6509280a21a3ab6fe97416979c758492e
Jonathan160993 <jonathan_aaron93@hotmail.com>
2023-06-29 00:21:13 -0600

    agregamos classes cuenta

---
+++ banco-heredado/src/Cuenta.java
@@ -0,0 +1,82 @@
+
+public class Cuenta {
+
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular = new Cliente();
+	
+	private static int total=0;
+	
+	public Cuenta (int agencia, int numero)
+	{
+		
+		if (agencia<=0 || this.numero!=numero) {
+			System.out.println("No se permite 0");
+			this.agencia=1;
+			this.numero= numero;
+		} else {
+			this.agencia= agencia;
+		}
+		total++;
+		System.out.println("Se va creando "+ total +" cuenta");
+	}
+//---------------------Metodo Depositar-----------------------------------------	
+	void depositar(double valor){
+	    this.saldo = this.saldo + valor;	
+	    System.out.println("Tu deposito de: $" +valor+ " pesos fue exitoso :D");
+	}
+	
+	
+//-------------------Metodo Consultar saldo-------------------------------------
+	void consultarSaldo() {
+		System.out.println("tu saldo actual es: $"+saldo);
+	}
+	
+	
+//---------------------Metodo Retirar-------------------------------------------
+	public boolean retirar (double monto){
+		
+		if (this.saldo >= monto) {
+		this.saldo = this.saldo - monto;
+		System.out.println("Retiro Exitoso ----- Retiraste: $"+monto);
+		return true;
+		} 
+			System.out.println("Saldo insuficiente");
+			return false;
+	}
+//--------------------Metodo Transferir-----------------------------------------
+	
+	public boolean tranferir(double monto, Cuenta cuenta){
+		
+		if (this.saldo >= monto) {
+			this.saldo = this.saldo - monto;
+			cuenta.saldo = cuenta.saldo + monto;
+			System.out.println("Transferencia Exitosa");
+			return true;
+		} 
+			System.out.println("No se pudo realizar tu operacion");
+			return false; 
+	}
+	
+//------------------Metodo ObtenerSaldo /Get ------------------------------------
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void  setAgencia(int agencia)
+	{
+		if (agencia > 0 ) {	
+		this.agencia = agencia; 
+		}
+	}
+	
+	public double getAgencia()
+	{
+		return this.agencia;
+	}
+}
---
+++ banco-heredado/src/CuentaAhorro.java
@@ -0,0 +1,11 @@
+
+public class CuentaAhorro extends Cuenta 
+{
+
+	public CuentaAhorro(int agencia, int numero) 
+	{
+		super(agencia, numero);
+		
+	}
+
+}
